12fb93242cb36546d60175cf3470a1e06eb59fc8
Adam Jędrych <adam.j@bytedance.com>
2019-10-09 21:41:49 +0200

    add transposition table

---
+++ engine/transposition_table.cpp
@@ -0,0 +1,34 @@
+#include "transposition_table.h"
+
+#include <cassert>
+#include <unordered_map>
+
+namespace engine
+{
+namespace transposition
+{
+
+const uint64_t HASH_MAP_SIZE = 1000000000;
+
+using HashMap = std::unordered_map<HashKey, Entry>;
+
+HashMap hashmap(HASH_MAP_SIZE);
+
+void update(HashKey key, Entry entry)
+{
+    hashmap.insert_or_assign(key, entry);
+}
+
+bool contains(HashKey key)
+{
+    return hashmap.find(key) != hashmap.end();
+}
+
+Entry get(HashKey key)
+{
+    assert(contains(key));
+    return hashmap[key];
+}
+
+}  // namespace transposition
+}  // namespace engine
