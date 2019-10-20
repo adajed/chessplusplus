1a0e2febfd41cac75e68407c2ff4ddda000eab64
Adam Jędrych <adam.jedrych25@gmail.com>
2019-10-20 12:04:31 +0200

    add some tests

--- tests/position_tests.cpp
+++ tests/position_tests.cpp
@@ -95,9 +95,63 @@ TEST(PositionTest, constructor_startpos)
     EXPECT_EQ(position.moves_counter(), 1);
 }
 
-TEST(PositionTest, pieces)
+TEST(PositionTest, constructor)
 {
-    Position position;
+    std::vector<std::string> fens = {
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 0 1",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1",
+    };
+
+    for (std::string fen : fens)
+    {
+        Position position(fen);
+        validate(position);
+
+        EXPECT_FALSE(position.is_checkmate());
+    }
+}
+
+TEST(PositionTest, fen)
+{
+    std::vector<std::string> fens = {
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 0 1",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1",
+    };
+
+    for (std::string fen : fens)
+    {
+        Position position(fen);
+        EXPECT_EQ(position.fen(), fen);
+    }
+}
+
+TEST(PositionTest, do_move)
+{
+    using TestCase = std::tuple<std::string, Move, MoveInfo, std::string>;
+
+    std::vector<TestCase> test_cases = {
+        {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+         create_move(SQ_E5, SQ_G6),
+         create_moveinfo(PAWN, ALL_CASTLING, NO_SQUARE, false),
+         "r3k2r/p1ppqpb1/bn2pnN1/3P4/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 1"
+        },
+    };
+
+    for (const TestCase& test_case : test_cases)
+    {
+        Position position(std::get<0>(test_case));
+        MoveInfo moveinfo = position.do_move(std::get<1>(test_case));
+        EXPECT_EQ(moveinfo, std::get<2>(test_case));
+        EXPECT_EQ(position.fen(), std::get<3>(test_case));
+    }
 }
 
 }
---
+++ tests/types_tests.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+
+#include "types.h"
+
+using namespace engine;
+
+namespace
+{
+
+TEST(TypesTest, create_move)
+{
+    for (Square from_sq = SQ_A1; from_sq <= SQ_H8; ++from_sq)
+    {
+        for (Square to_sq = SQ_A1; to_sq <= SQ_H8; ++to_sq)
+        {
+            Move move = create_move(from_sq, to_sq);
+            EXPECT_EQ(from(move), from_sq);
+            EXPECT_EQ(to(move), to_sq);
+            EXPECT_EQ(promotion(move), NO_PIECE_KIND);
+            EXPECT_EQ(castling(move), NO_CASTLING);
+        }
+    }
+}
+
+TEST(TypesTest, create_promotion)
+{
+    for (Square from_sq = SQ_A1; from_sq <= SQ_H8; ++from_sq)
+    {
+        for (Square to_sq = SQ_A1; to_sq <= SQ_H8; ++to_sq)
+        {
+            for (PieceKind piecekind = KNIGHT; piecekind <= QUEEN; ++piecekind)
+            {
+                Move move = create_promotion(from_sq, to_sq, piecekind);
+                EXPECT_EQ(from(move), from_sq);
+                EXPECT_EQ(to(move), to_sq);
+                EXPECT_EQ(promotion(move), piecekind);
+                EXPECT_EQ(castling(move), NO_CASTLING);
+            }
+        }
+    }
+}
+
+TEST(TypesTest, create_castling)
+{
+    for (Castling c : {KING_CASTLING, QUEEN_CASTLING})
+    {
+        Move move = create_castling(c);
+        EXPECT_EQ(castling(move), c);
+    }
+}
+
+TEST(TypesTest, create_moveinfo)
+{
+    for (uint32_t piecekind = NO_PIECE_KIND; piecekind <= QUEEN; ++piecekind)
+    {
+        for (uint32_t castling = NO_CASTLING; castling <= ALL_CASTLING; ++castling)
+        {
+            for (Square square = SQ_A1; square <= NO_SQUARE; ++square)
+            {
+                for (bool b : {false, true})
+                {
+                    MoveInfo moveinfo = create_moveinfo(PieceKind(piecekind), Castling(castling), square, b);
+                    EXPECT_EQ(captured_piece(moveinfo), PieceKind(piecekind));
+                    EXPECT_EQ(last_castling(moveinfo), Castling(castling));
+                    EXPECT_EQ(last_enpassant(moveinfo), square != NO_SQUARE);
+                    if (square != NO_SQUARE)
+                        EXPECT_EQ(last_enpassant_square(moveinfo), square);
+                    EXPECT_EQ(enpassant(moveinfo), b);
+                }
+            }
+        }
+    }
+}
+
+}
