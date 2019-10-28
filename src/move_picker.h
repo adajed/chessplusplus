9a8ca33420f7e576de332a22e27ad6bd22c05e34
Adam Jędrych <adam.jedrych25@gmail.com>
2019-10-28 14:00:28 +0100

    add ordering info (#18)

--- src/move_picker.h
+++ src/move_picker.h
@@ -9,10 +9,41 @@
 namespace engine
 {
 
+struct OrderingInfo
+{
+    public:
+        OrderingInfo()
+        {
+            for (int i = 0; i < 50; ++i)
+                killers[i][0] = killers[i][1] = NO_MOVE;
+            for (int c = 0; c < 2; ++c)
+                for (int from = 0; from < SQUARE_NUM; ++from)
+                    for (int to = 0; to < SQUARE_NUM; ++to)
+                        history[c][from][to] = 0;
+            ply = 0;
+        }
+
+        void update_killers(int p, Move move)
+        {
+            killers[p][1] = killers[p][0];
+            killers[p][0] = move;
+        }
+
+        void update_history(Color c, Square from, Square to, int depth)
+        {
+            history[c][from][to] += depth * depth;
+        }
+
+        Move killers[50][2];
+        int history[COLOR_NUM][SQUARE_NUM][SQUARE_NUM];
+        int ply;
+};
+
+
 class MovePicker
 {
     public:
-        MovePicker(const Position& position, Move* begin, Move* end);
+        MovePicker(const Position& position, Move* begin, Move* end, OrderingInfo& info, bool use_info);
 
         bool has_next();
 
@@ -20,7 +51,7 @@ class MovePicker
 
     private:
 
-        uint32_t score_move(const Position& position, Move move);
+        uint32_t score_move(const Position& position, Move move, OrderingInfo& info, bool use_info);
 
         Move* begin;
         Move* end;
