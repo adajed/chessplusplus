a48a9760b7b52fb69a1100f76ad62278d86aa01b
Adam Jędrych <adam.jedrych25@gmail.com>
2019-11-01 21:51:56 +0100

    Hardcode weights (#25)
    * Hardcode weights
    
    * remove weights.txt

--- src/score.h
+++ src/score.h
@@ -10,16 +10,20 @@ namespace engine
 class PositionScorer
 {
     public:
-        explicit PositionScorer(const Weights& weights);
+        explicit PositionScorer();
 
         int64_t score(const Position& position) const;
 
     private:
 
         template <Color side>
+        int64_t score_side(const Position& position) const;
+
+        template <Color side, GamePhase phase>
         int64_t score(const Position& position) const;
 
-        Weights weights;
+        int64_t game_phase_weight(const Position& position) const;
+
         Move move_list[MAX_MOVES];
 };
 
