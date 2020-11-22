3139b3164c06b145b44b9deaf009a93fc92b1a84
Adam Jędrych <adam.jedrych25@gmail.com>
2020-11-22 23:40:19 +0100

    Fix tt (#50)
    * Fix transposition table
    
    * Remove include<algorithm>
    
    * Fix tests
    
    * Fix travis.yml

--- src/move_picker.cpp
+++ src/move_picker.cpp
@@ -5,77 +5,85 @@
 namespace engine
 {
 
+
+// bonus for caputure
+// first dim is PieceKind of captured piece
+// second dim is PieceKind of capturing piece
+const uint32_t CAPTURE_BONUS[PIECE_KIND_NUM][PIECE_KIND_NUM] = {
+    {},
+    {0, 6, 5, 4, 3, 2, 1},          // captured pawn
+    {0, 12, 11, 10, 9, 8, 7},       // captured knight
+    {0, 18, 17, 16, 15, 14, 13},    // captured bishop
+    {0, 24, 23, 22, 21, 20, 19},    // captured rook
+    {0, 30, 29, 28, 27, 26, 25},    // captured queen
+    {}
+};
+
+// bonus for promotion
+const uint32_t PROMOTION_BONUS[PIECE_KIND_NUM] = {
+    0,
+    0,
+    1, // knight
+    2, // bishop
+    3, // rook
+    4, // queen
+    0};
+
+
 MovePicker::MovePicker(const Position& position, Move* begin, Move* end, OrderingInfo& info, bool use_info)
-    : begin(begin), end(end), pos(0), scores(end - begin)
+    : _moves(end - begin), _pos(0)
 {
     size_t n = end - begin;
     for (size_t i = 0; i < n; ++i)
-        scores[i] = score_move(position, begin[i], info, use_info);
+        _moves[i] = std::make_pair(0, begin[i]);
+
+    score_moves(position, info, use_info);
+
+    std::sort(_moves.begin(), _moves.end(), std::greater<>());
 }
 
 bool MovePicker::has_next()
 {
-    size_t n = end - begin;
-    return pos < n;
+    return _pos < _moves.size();
 }
 
 Move MovePicker::get_next()
 {
-    size_t n = end - begin;
-    uint32_t best_score = scores[pos];
-    size_t best_index = pos;
-    for (size_t i = pos + 1; i < n; ++i)
-    {
-        if (scores[i] > best_score)
-        {
-            best_score = scores[i];
-            best_index = i;
-        }
-    }
-
-    Move ret = begin[best_index];
-    begin[best_index] = begin[pos];
-    scores[best_index] = scores[pos];
-    begin[pos] = ret;
-
-    pos++;
-    return ret;
+    return _moves[_pos++].second;
 }
 
-uint32_t MovePicker::score_move(const Position& position, Move move, OrderingInfo& info, bool use_info)
+void MovePicker::score_moves(const Position& position, OrderingInfo& info, bool use_info)
 {
-    PieceKind moved_piece = make_piece_kind(position.piece_at(from(move)));
-    PieceKind captured_piece = make_piece_kind(position.piece_at(to(move)));
-    PieceKind promotion_piece = promotion(move);
-    Move pv_move = NO_MOVE;
-    if (info.pv_moves.find(position.hash()) != info.pv_moves.end())
-        pv_move = info.pv_moves[position.hash()];
+    Move pvMove = NO_MOVE;
+    if (use_info)
+    {
+        const tt::TTEntry* entryPtr = info.ttable.get(position.hash());
+        if (entryPtr)
+            pvMove = entryPtr->move;
+    }
+    for (uint32_t i = 0; i < _moves.size(); ++i)
+    {
+        Move move = _moves[i].second;
 
-    const uint32_t capture_bonus[PIECE_KIND_NUM][PIECE_KIND_NUM] = {
-        {},
-        {0, 6, 5, 4, 3, 2, 1},
-        {0, 12, 11, 10, 9, 8, 7},
-        {0, 18, 17, 16, 15, 14, 13},
-        {0, 24, 23, 22, 21, 20, 19},
-        {0, 30, 29, 28, 27, 26, 25},
-        {}
-    };
+        PieceKind moved_piece = make_piece_kind(position.piece_at(from(move)));
+        PieceKind captured_piece = make_piece_kind(position.piece_at(to(move)));
+        PieceKind promotion_piece = promotion(move);
 
-    const uint32_t promotion_bonus[PIECE_KIND_NUM] = {0, 0, 1, 2, 3, 4, 0};
-
-    if (move == pv_move)
-        return 30000;
-    if (captured_piece != NO_PIECE_KIND)
-        return 29000 + capture_bonus[captured_piece][moved_piece];
-    if (promotion_piece != NO_PIECE_KIND)
-        return 28000 + promotion_bonus[promotion_piece];
-    if (!use_info)
-        return 1;
-    if (move == info.killers[info.ply][0])
-        return 27999;
-    if (move == info.killers[info.ply][1])
-        return 27998;
-    return 1 + info.history[position.side_to_move()][from(move)][to(move)];
+        if (move == pvMove)
+            _moves[i].first = 1000000;
+        else if (captured_piece != NO_PIECE_KIND)
+            _moves[i].first = 30000 + CAPTURE_BONUS[captured_piece][moved_piece];
+        else if (promotion_piece != NO_PIECE_KIND)
+            _moves[i].first = 29000 + PROMOTION_BONUS[promotion_piece];
+        else if (!use_info)
+            _moves[i].first = 1;
+        else if (move == info.killers[info.ply][0])
+            _moves[i].first = 28000;
+        else if (move == info.killers[info.ply][1])
+            _moves[i].first = 27000;
+        else
+            _moves[i].first = 1 + info.history[position.side_to_move()][from(move)][to(move)];
+    }
 }
 
 }  // namespace engine
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,14 @@
 #include <gtest/gtest.h>
 
 #include "move_bitboards.h"
+#include "zobrist_hash.h"
 
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
+
     engine::init_move_bitboards();
+    engine::zobrist::init();
+
     return RUN_ALL_TESTS();
 }
