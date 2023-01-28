88d40e55fe14e9aead9b9a6cb568e7982e5f0b9e
Adam Jędrych <adam.jedrych25@gmail.com>
2023-01-28 17:06:43 +0100

    [perft] Additional perft tests

--- tests/run_perft_tests.sh
+++ tests/run_perft_tests.sh
@@ -78,6 +78,29 @@ run_perft_test "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w 
 run_perft_test "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - " 4 3894594
 run_perft_test "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - " 5 164075551
 
+run_perft_test "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2" 1 8
+run_perft_test "8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3" 1 8
+run_perft_test "r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2" 1 19
+run_perft_test "r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2" 1 5
+run_perft_test "2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2" 1 44
+run_perft_test "rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9" 1 39
+run_perft_test "2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4" 1 9
+run_perft_test "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8" 3 62379
+run_perft_test "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10" 3 89890
+run_perft_test "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1" 6 1134888
+run_perft_test "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1" 6 1015133
+run_perft_test "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1" 6 1440467
+run_perft_test "5k2/8/8/8/8/8/8/4K2R w K - 0 1" 6 661072
+run_perft_test "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1" 6 803711
+run_perft_test "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1" 4 1274206
+run_perft_test "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1" 4 1720476
+run_perft_test "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1" 6 3821001
+run_perft_test "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1" 5 1004658
+run_perft_test "4k3/1P6/8/8/8/8/K7/8 w - - 0 1" 6 217342
+run_perft_test "8/P1k5/K7/8/8/8/8/8 w - - 0 1" 6 92683
+run_perft_test "K1k5/8/P7/8/8/8/8/8 w - - 0 1" 6 2217
+run_perft_test "8/k1P5/8/1K6/8/8/8/8 w - - 0 1" 7 567584
+run_perft_test "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1" 4 23527
 
 echo "Perft tests passed: ${PERFT_TESTS_PASSED}/${NUMBER_OF_PERFT_TESTS}"
 if [ ${PERFT_TESTS_PASSED} -ne ${NUMBER_OF_PERFT_TESTS} ]
